2210561ef44c4adb82e403b5cad9aea5d0018995
doniel-t <daniel.theil.g@gmail.com>
2023-02-13 20:19:55 +0100

    clarified var naming

--- media_control_server/main.py
+++ media_control_server/main.py
@@ -1,4 +1,4 @@
-from media_control import press_media_control_key, MEDIA_KEYS_HEX
+from media_control import press_media_control_key, MEDIA_KEYS
 from fastapi import FastAPI
 
 
@@ -9,14 +9,13 @@ if __name__ == 'main':
     @app.get("/media_controls/{control_key}")
     def play(control_key):
         try:
-            print(control_key)
-            press_media_control_key(MEDIA_KEYS_HEX[control_key])
+            press_media_control_key(MEDIA_KEYS[control_key])
             return {
                 "message": f"pressed {control_key}",
                 "status" : 200
             }
         except:
-            available_endpoints = list(MEDIA_KEYS_HEX.keys())
+            available_endpoints = list(MEDIA_KEYS.keys())
             return {
                 "status": 400,
                 "message": f"A normal request looks like this: hostname:port/media_controls/endpoint \nYou can try the following endpoints: {available_endpoints}"
--- media_control_server/media_control.py
+++ media_control_server/media_control.py
@@ -2,7 +2,7 @@ import win32api
 import win32con
 
 
-MEDIA_KEYS_HEX = {
+MEDIA_KEYS = {
     'play_pause' : win32con.VK_MEDIA_PLAY_PAUSE,
     'next' : win32con.VK_MEDIA_NEXT_TRACK,
     'prev' : win32con.VK_MEDIA_PREV_TRACK,
@@ -20,3 +20,5 @@ def press_media_control_key(key):
         #change volume by 6% instead of 2% for better UX
         for _ in range(3):
             win32api.keybd_event(key, 0)
+
+
